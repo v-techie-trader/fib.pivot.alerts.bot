36408d77ecf55906880fa39a9bc72e2bf7ef7fcd
v-techie-trader <v.techie-trader@gmail.com>
2022-01-07 15:08:32 +0530

    Initial setup

---
+++ config.py
@@ -0,0 +1,51 @@
+# ----------------------------------------------- #
+# Plugin Name           : TradingView-Webhook-Bot #
+# Author Name           : fabston                 #
+# File Name             : config.py               #
+# ----------------------------------------------- #
+
+# TradingView Example Alert Message:
+# {
+# "key":"9T2q394M92", "telegram":"-1001298977502", "discord":"789842349670960670/BFeBBrCt-w2Z9RJ2wlH6TWUjM5bJuC29aJaJ5OQv9sE6zCKY_AlOxxFwRURkgEl852s3", "msg":"Long #{{ticker}} at `{{close}}`"
+# }
+
+sec_key1 = (
+    "lenh1"  # Can be anything. Has to match with "key" in your TradingView alert message. Lenh cancel, close
+)
+sec_key2 = (
+    "lenh2"  # Can be anything. Has to match with "key" in your TradingView alert message. lenh order trade
+)
+# Telegram Settings
+send_telegram_alerts = True
+tg_token = "5089014574:AAGa7J9DqvM7I0Rxt7t5grxtKzF0uTwq7xE"  # Bot token. Get it from @Botfather
+channel =  -1001620765727 # Channel ID (ex. -1001487568087)
+
+# Discord Settings
+send_discord_alerts = False
+discord_webhook = ""  # Discord Webhook URL (https://support.discordapp.com/hc/de/articles/228383668-Webhooks-verwenden)
+
+# Slack Settings
+send_slack_alerts = False
+slack_webhook = ""  # Slack Webhook URL (https://api.slack.com/messaging/webhooks)
+
+# Curl Settings
+send_curl_alerts = True
+curl_webhook = "http://127.0.0.1:8888/frostybot"  
+
+# Twitter Settings
+send_twitter_alerts = False
+tw_ckey = ""
+tw_csecret = ""
+tw_atoken = ""
+tw_asecret = ""
+
+# Email Settings
+send_email_alerts = False
+email_sender = ""  # Your email address
+email_receivers = ["", ""]  # Receivers, can be multiple
+email_subject = "Trade Alert!"
+
+email_port = 465  # SMTP SSL Port (ex. 465)
+email_host = ""  # SMTP host (ex. smtp.gmail.com)
+email_user = ""  # SMTP Login credentials
+email_password = ""  # SMTP Login credentials
---
+++ main.py
@@ -0,0 +1,85 @@
+# ----------------------------------------------- #
+# Plugin Name           : TradingView-Webhook-Bot #
+# Author Name           : fabston                 #
+# File Name             : main.py                 #
+# ----------------------------------------------- #
+
+import json
+import time
+import logging
+from flask import Flask, request
+
+import config
+from handler import *
+
+app = Flask(__name__)
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+def get_timestamp():
+    timestamp = time.strftime("%Y-%m-%d %X")
+    return timestamp
+
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    try:
+        if request.method == "POST":
+            data = request.get_json()
+            key = data["key"]
+            sleeptime = data["sleeptime"]
+            sleeptime1 = int(sleeptime)
+            if key == config.sec_key1:
+                print(get_timestamp(), "Alert Received & Sent!")
+                send_alert(data)
+                return "Sent alert", 200
+            elif key == config.sec_key2:
+                time.sleep(sleeptime1)
+                print(get_timestamp(), "Alert Received & Sent!")
+                send_alert(data)
+                return "Sent alert", 200
+            else:
+                
+                print("[X]", get_timestamp(), "Alert Received & Refused! (Wrong Key)")
+                return "Refused alert", 400
+
+    except Exception as e:
+        print("[X]", get_timestamp(), "Error:\n>", e)
+        return "Error", 400
+
+@app.route("/webhook1", methods=["POST"])
+def webhook1():
+    try:
+        if request.method == "POST":
+            data1 = request.get_json()
+            key1 = data["key"]
+            sleeptime2 = data["sleeptime"]
+            sleeptime3 = int(sleeptime2)
+            if key1 == config.sec_key1:
+                print(get_timestamp(), "Alert Received & Sent!")
+                send_alert(data)
+                return "Sent alert", 200
+            elif key1 == config.sec_key2:
+                time.sleep(sleeptime3)
+                print(get_timestamp(), "Alert Received & Sent!")
+                send_alert(data)
+                return "Sent alert", 200
+            else:
+                
+                print("[X]", get_timestamp(), "Alert Received & Refused! (Wrong Key)")
+                return "Refused alert", 400
+
+    except Exception as e:
+        print("[X]", get_timestamp(), "Error:\n>", e)
+        return "Error", 400
+
+@app.route("/", methods=["GET"])
+def get():
+    return "fib.pivot.alerts.bot Running" , 200
+
+if __name__ == "__main__":
+    from waitress import serve
+    logger.info("Started")
+    serve(app, host="0.0.0.0", port=80)
